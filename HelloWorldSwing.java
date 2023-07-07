63b878fbaaefa65545ebbd759063ba357a252dcf
Real G <namanh8t@gmail.com>
2023-07-07 12:06:44 +0700

    Save

--- HelloWorldSwing.java
+++ HelloWorldSwing.java
@@ -1,14 +1,26 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.awt.*;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
+// class DrawRec extend JPanel implement MouseListener, MouseMotionListener{
+//     private List<Point> points;
+
+//     DrawRec(){
+//         points =new ArrayList<>();
+//         addMouseListener(this);
+//         addMouseMotionListener(this);
+//     }
+
+//     public void mouseDragged(MouseEvent e){
+         
+//     }
+// }
 
 class Draw extends JPanel implements MouseListener, MouseMotionListener {
     private List<Point> points;
@@ -24,14 +36,6 @@ class Draw extends JPanel implements MouseListener, MouseMotionListener {
 
     public Dimension getPreferredSize() {
         return new Dimension(600, 600);
-    }
-
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.setColor(Color.BLACK);
-        for (Point point : points) {
-            g.fillOval(point.x, point.y, 10, 10);
-        }
     }
 
     public void mousePressed(MouseEvent e) {
@@ -52,6 +56,15 @@ class Draw extends JPanel implements MouseListener, MouseMotionListener {
             int mouseY = e.getY();
             points.add(new Point(mouseX, mouseY));
             repaint();
+        }
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(Color.BLACK);
+        for (Point point : points) {
+            g2d.fillOval(point.x, point.y, 10, 10);
         }
     }
 
@@ -76,43 +89,61 @@ public class HelloWorldSwing {
     private static void createAndShowGUI() {
         JFrame frame = new JFrame("Hello World Swing");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1080, 720);
+        frame.setSize(800, 600);
         frame.setVisible(true);
 
-        //test
-        frame.setBackground(Color.red);
-
-
-        //MenuBar
+        // MenuBar
         JMenuBar menuBar = new JMenuBar();
         frame.setJMenuBar(menuBar);
-        JMenu menuFiles= new JMenu("Files");
+        JMenu menuFiles = new JMenu("Files");
         menuBar.add(menuFiles);
-        JMenuItem itemOpen = new JMenuItem("Open",KeyEvent.VK_0);
+        JMenuItem itemOpen = new JMenuItem("Open", KeyEvent.VK_0);
         menuFiles.add(itemOpen);
 
         itemOpen.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
                 System.out.println("Open what?");
             }
         });
 
-  
-
-        JMenuItem itemSave= new JMenuItem("Save", 0);
+        JMenuItem itemSave = new JMenuItem("Save", 0);
         menuFiles.add(itemSave);
-        JMenuItem itemSaveas=new JMenuItem("Save as", 0);
-        menuFiles.add(itemSaveas);
+
+        itemSave.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser saveFileChooser = new JFileChooser();
+                int returnValue = saveFileChooser.showSaveDialog(frame);
+
+                if (returnValue == JFileChooser.APPROVE_OPTION) {
+                    String filePath = saveFileChooser.getSelectedFile().getPath() + ".png";
+
+                    BufferedImage image = new BufferedImage(frame.getContentPane().getWidth(),
+                            frame.getContentPane().getHeight(), BufferedImage.TYPE_INT_ARGB);
+                    Graphics2D g2d = image.createGraphics();
+                    frame.getContentPane().getComponent(0).paint(g2d);
+                    g2d.dispose();
+
+                    try {
+                        ImageIO.write(image, "PNG", new File(filePath));
+                        System.out.println("Lưu tệp tin vào đường dẫn: " + filePath);
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        JMenuItem itemSaveAs = new JMenuItem("Save as", 0);
+        menuFiles.add(itemSaveAs);
         menuFiles.addSeparator();
-        JMenuItem itemClose= new JMenuItem("Close", 0);
+        JMenuItem itemClose = new JMenuItem("Close", 0);
         menuFiles.add(itemClose);
 
         // Content
         JPanel draw = new Draw();
         frame.getContentPane().add(draw);
-
-   
     }
 
     public static void main(String[] args) {
